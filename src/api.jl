475ff990471ace7cc01c515f5c2c31ca91c43d94
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2020-02-11 23:37:18 -0700

    use API function in tests

--- src/api.jl
+++ src/api.jl
@@ -1,18 +1,16 @@
-function construct_milp_relaxation(f::Function, partition::Vector{<:Real})::FormulationData
-    return construct_milp_relaxation(f, x -> ForwardDiff(f, x), partition)
+function construct_milp_relaxation(f::Function, partition::Vector{<:Real};
+        error_tolerance::Float64=Inf,
+        num_additional_binary_variables::Int=0)::FormulationData
+    return construct_milp_relaxation(
+        f,
+        x -> ForwardDiff.derivative(f, x),
+        partition,
+        error_tolerance=error_tolerance,
+        num_additional_binary_variables=num_additional_binary_variables)
 end
 
-function construct_milp_relaxation(f::Function, d::Function, partition::Vector{<:Real})::FormulationData
-    return build_model(FunctionData(f, d, partition))
+function construct_milp_relaxation(f::Function, d::Function,
+        partition::Vector{<:Real}; error_tolerance=Inf,
+        num_additional_binary_variables::Int=0)::FormulationData
+    return build_formulation(FunctionData(f, d, partition))
 end
-
-# function construct_relaxation(function_data::FunctionData;
-#     partition_points::Vector{Float64}=[],
-#     error_tolerance::Float64=Inf,
-#     binary_variable_budget::Int=0)
-
-#     if (length(partition_points) == 0 && isinf(error_tolerance) &&
-#         binary_variable_budget == 0)
-#         return build_model(uf)
-#     end
-# end
