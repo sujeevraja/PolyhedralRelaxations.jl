424d9e29a9f12cf99f10418c760d010349c834cc
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2020-02-11 18:39:25 -0700

    simplify function input

--- src/types.jl
+++ src/types.jl
@@ -1,29 +1,21 @@
-struct UnivariateFunction
+struct FunctionData
     f::Function
     f_dash::Function
-    domain_lb::Real
-    domain_ub::Real
-    inflection_points::Vector{Real}
+    partition::Vector{Real}
 end
 
-"Constructors for UnivariateFunction"
-function UnivariateFunction(f::Function, f_dash::Function; domain_lb::Real=-Inf, domain_ub::Real=Inf, inflection_points::Vector{Real}=[])::UnivariateFunction
-    if isinf(domain_lb) || isinf(domain_ub)
-        Memento.error(_LOGGER, "the univariate function's domain has to be a closed interval; please specify the bounds using the domain_lb and domain_ub keyword arguments")
-    end
-    return UnivariateFunction(f, f_dash, domain_lb, domain_ub, inflection_points)
-end
-UnivariateFunction(f::Function; domain_lb::Real=-Inf, domain_ub::Real=Inf, inflection_points::Vector{Real}=[]) =
-    UnivariateFunction(f, x -> ForwardDiff.derivative(f, x), domain_lb=domain_lb, domain_ub=domain_ub, inflection_points=inflection_points)
-UnivariateFunction(f::Function, f_dash::Function, lb, ub) =  UnivariateFunction(f, f_dash, domain_lb=lb, domain_ub=ub)
+"Constructor without definition of derivative"
+FunctionData(f::Function, partition::Vector{Real}) =
+    FunctionData(f, x -> ForwardDiff.derivative(f, x), partition)
 
 "Getters for UnivariateFunction"
-@inline get_function(uf::UnivariateFunction)::Function = uf.f
-@inline get_derivative(uf::UnivariateFunction)::Function = uf.f_dash
-@inline get_domain_lb(uf::UnivariateFunction)::Real = uf.domain_lb
-@inline get_domain_ub(uf::UnivariateFunction)::Real = uf.domain_ub
-@inline get_domain(uf::UnivariateFunction)::Tuple{Real,Real} = get_domain_lb(uf), get_domain_ub(uf)
-@inline get_inflection_points(uf::UnivariateFunction)::Vector{<:Real} = uf.inflection_points
+@inline get_function(uf::FunctionData)::Function = uf.f
+@inline get_derivative(uf::FunctionData)::Function = uf.f_dash
+@inline get_domain_lb(uf::FunctionData)::Real = uf.partition[1]
+@inline get_domain_ub(uf::FunctionData)::Real = uf.partition[end]
+@inline get_domain(uf::FunctionData)::Pair{Real,Real} =
+    get_domain_lb(uf), get_domain_ub(uf)
+@inline get_partition(uf::FunctionData)::Vector{<:Real} = uf.partition
 
 const Vertex = Pair{Real,Real}
 
@@ -92,7 +84,7 @@ function IndexData(num_points::Int64)::IndexData
     # variables with indices 3,4. As the collect() function includes both
     # endpoints, we need to set num_vars to k-2. Then, we will get the correct
     # count for each variable set.
-    
+
     num_vars = num_points - 2
 
     start = 3
