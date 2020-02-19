0c36f02413484fe9ecfc5502c50b12119344574f
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2020-02-19 16:34:46 -0700

    cleanup

--- test/linear_objective_tests.jl
+++ test/linear_objective_tests.jl
@@ -1,9 +1,9 @@
 @testset "linear objective function tests" begin
     PR.logger_config!("error")
-    milp_relaxation, milp_function_data = construct_milp_relaxation(x -> x^3, collect(-1.0:0.05:1.0))
-    lp_relaxation, lp_function_data = construct_lp_relaxation(x -> x^3, collect(-1.0:0.05:1.0))
-    
-    # create LP relaxation of the MILP relaxation of the univariate function
+    f, partition = x -> x^3, collect(-1.0:0.05:1.0)
+
+    # create LP relaxation of the MILP relaxation of the univariate function.
+    milp_relaxation, milp_function_data = construct_milp_relaxation(f, partition)
     milp = Model(glpk_optimizer)
     lb, ub = get_variable_bounds(milp_relaxation)
     num_variables = get_num_variables(milp_relaxation)
@@ -12,8 +12,9 @@
     @constraint(milp, A * x .== b)
     A, b = get_leq_constraint_matrices(milp_relaxation)
     @constraint(milp, A * x .<= b)
-    
-    # create LP relaxation of the univariate function 
+
+    # create LP relaxation of the univariate function using the convex hull formulation.
+    lp_relaxation, lp_function_data = construct_lp_relaxation(f, partition)
     lp = Model(glpk_optimizer)
     lb, ub = get_variable_bounds(lp_relaxation)
     num_variables = get_num_variables(lp_relaxation)
@@ -27,13 +28,13 @@
         @objective(lp, Min, y[lp_relaxation.y_index] - tan(α) * y[lp_relaxation.x_index])
         optimize!(milp)
         optimize!(lp)
-        @test isapprox(objective_value(milp), objective_value(lp), atol=1e-5)
+        @test objective_value(milp) ≈ objective_value(lp) atol=1e-5
 
-        @objective(milp, Max, x[milp_relaxation.y_index] - tan(α) * x[milp_relaxation.x_index])
-        @objective(lp, Max, y[lp_relaxation.y_index] - tan(α) * y[lp_relaxation.x_index])
+        set_objective_sense(milp, MOI.MAX_SENSE)
+        set_objective_sense(lp, MOI.MAX_SENSE)
         optimize!(milp)
         optimize!(lp)
-        @test isapprox(objective_value(milp), objective_value(lp), atol=1e-5)
+        @test objective_value(milp) ≈ objective_value(lp) atol=1e-5
     end
 
     # sec_verts, tan_verts = collect_vertices(function_data)
