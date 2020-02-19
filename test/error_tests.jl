ffccf0e4e6fecac574e8851daf6b905806e92290
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-02-18 20:00:11 -0700

    formatting fix

--- test/error_tests.jl
+++ test/error_tests.jl
@@ -1,12 +1,24 @@
 @testset "test for errors" begin
     PR.logger_config!("error")
     logger = Memento.getlogger(PolyhedralRelaxations)
-    @test_throws(logger, ErrorException, construct_milp_relaxation(x->x^2, [0.0]))
-    @test_throws(logger, ErrorException, construct_milp_relaxation(x->x^2, [0.0, 1e16]))
-    @test_throws(logger, ErrorException, construct_milp_relaxation(x->tan(x), [0.0, pi/2]))
-    @test_throws(logger, ErrorException, construct_milp_relaxation(x->sin(x), [0.0, 2*pi]))
-    @test_throws(logger, ErrorException, construct_milp_relaxation(x->x^3, [0.0, -1.0, 1.0]))
-    lp, lp_function_data = construct_lp_relaxation(x->x^3, collect(-1.0:1.0:1.0))
+    @test_throws(logger, ErrorException, construct_milp_relaxation(x -> x^2, [0.0]))
+    @test_throws(logger, ErrorException, construct_milp_relaxation(x -> x^2, [0.0, 1e16]))
+    @test_throws(
+        logger,
+        ErrorException,
+        construct_milp_relaxation(x -> tan(x), [0.0, pi / 2])
+    )
+    @test_throws(
+        logger,
+        ErrorException,
+        construct_milp_relaxation(x -> sin(x), [0.0, 2 * pi])
+    )
+    @test_throws(
+        logger,
+        ErrorException,
+        construct_milp_relaxation(x -> x^3, [0.0, -1.0, 1.0])
+    )
+    lp, lp_function_data = construct_lp_relaxation(x -> x^3, collect(-1.0:1.0:1.0))
     @test_throws(logger, ErrorException, get_geq_constraint_matrices(lp))
     @test_throws(logger, ErrorException, get_leq_constraint_matrices(lp))
-end 
+end
--- test/sampling_tests.jl
+++ test/sampling_tests.jl
@@ -1,15 +1,18 @@
-get_milp_xcube(;error_tolerance=1e-2) = 
-    construct_milp_relaxation(x -> x^3, collect(-1.0:1.0:1.0), error_tolerance = error_tolerance)
+get_milp_xcube(; error_tolerance = 1e-2) = construct_milp_relaxation(
+    x -> x^3,
+    collect(-1.0:1.0:1.0),
+    error_tolerance = error_tolerance,
+)
 
 @testset "sampling tests for error tolerance" begin
     PR.logger_config!("error")
-    milp, milp_function_data = get_milp_xcube() 
+    milp, milp_function_data = get_milp_xcube()
     lb, ub = get_domain(milp_function_data)
     var_lb, var_ub = get_variable_bounds(milp)
     num_variables = get_num_variables(milp)
-    for i=1:10
+    for i = 1:10
         λ = rand()
-        x_val = λ * lb + (1 - λ) * ub 
+        x_val = λ * lb + (1 - λ) * ub
         m = Model(glpk_optimizer)
         @variable(
             m,
@@ -30,7 +33,7 @@ get_milp_xcube(;error_tolerance=1e-2) =
         @objective(m, Max, x[milp.y_index])
         optimize!(m)
         y_max = objective_value(m)
-        @test abs(y_max-y_min) <= 1e-2
-    end 
+        @test abs(y_max - y_min) <= 1e-2
+    end
 
-end 
+end
