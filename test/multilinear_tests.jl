775256b488cb5702197fa0d26208473224d28aee
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2022-08-26 15:05:48 -0600

    more tests for linking constraints

--- test/multilinear_tests.jl
+++ test/multilinear_tests.jl
@@ -84,4 +84,38 @@
         @test objective_value(m) <= gopt_value
         @test !isempty(formulation_info.extra[:common_subterm_data])
     end
+
+    @testset "test multilinear MILP relaxation with linking constraints" begin
+        PR.silence!()
+        num_discretizations = [3, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        instance =
+            create_multilinear_model(num_discretizations = num_discretizations)
+        m = instance.model
+        x = instance.variables[:x]
+        y = instance.variables[:y]
+        partitions = instance.partitions
+        multilinear_terms = instance.multilinear_terms
+        info = Dict(
+            last(term) => construct_multilinear_relaxation!(
+                m,
+                last(term),
+                first(term),
+                partitions,
+            ) for term in multilinear_terms
+        )
+        gopt_value = instance.objective
+        formulation_info =
+            add_multilinear_linking_constraints!(m, info, partitions)
+        set_optimizer(m, milp_optimizer)
+        optimize!(m)
+        relaxation_obj = round(objective_value(m); digits = 4)
+        gap = round(
+            abs(relaxation_obj - gopt_value) / abs(gopt_value) * 100.0;
+            digits = 2,
+        )
+        time = round(solve_time(m); digits = 2)
+        @info "Relative gap (MILP with linking constraints): $(gap)%, time: $(time) sec."
+        @test objective_value(m) <= gopt_value
+        @test !isempty(formulation_info.extra[:common_subterm_data])
+    end
 end
